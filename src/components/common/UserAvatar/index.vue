46a5c66f75a380e59555bbbef3097e9857c69faf
wrtpi <71971470+wrtpi@users.noreply.github.com>
2023-02-27 02:09:08 +0800

    Update index.vue

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex items-center">
     <div class="w-10 h-10 overflow-hidden rounded-full">
-      <img class="object-cover" src="@/assets/avatar.jpg" alt="avatar">
+      <img class="object-cover" src="@/assets/avatar.png" alt="avatar">
     </div>
     <div class="ml-2">
       <h2 class="font-bold text-md">
-        JunJun
+        JunJun俊俊
       </h2>
     </div>
   </div>
